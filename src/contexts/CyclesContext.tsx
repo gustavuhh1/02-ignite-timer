9725af6a5e955f1b186cfc5397850e517b520529
GustavoMartinsR <gustavomrodrigues11@gmail.com>
2025-01-11 15:21:50 -0300

    first commit

---
+++ src/contexts/CyclesContext.tsx
@@ -0,0 +1,110 @@
+import { createContext, ReactNode, useState, useReducer, useEffect } from "react";
+import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
+import {
+  addNewCycleAction,
+  interruptCurrentCycleAction,
+  markCurrentCycleAsFinishedAction,
+} from "../reducers/cycles/actions";
+import { differenceInSeconds } from "date-fns";
+
+interface CreateCycleDate {
+  task: string;
+  minutesAmount: number;
+}
+
+interface CyclesContextType {
+  cycles: Cycle[];
+  activeCycle: Cycle | undefined;
+  activeCycleId: string | null;
+  markCurrentCycleAsFinish: () => void;
+  amountSecondsPassed: number;
+  setSecondsPassed: (seconds: number) => void;
+  createNewCycle: (data: CreateCycleDate) => void;
+  interruptCycle: () => void;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const CyclesContext = createContext({} as CyclesContextType);
+
+interface CyclesContextProviderProps {
+  children: ReactNode;
+}
+
+export function CyclesContextProvider({ children }: CyclesContextProviderProps) {
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (initialState) => {
+      const storedStateAsJSON = localStorage.getItem("@ignite-timer:cycles-state-1.0.0");
+
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON);
+      }
+
+      return { cycles: [], activeCycleId: null };
+    }
+  );
+  const { cycles, activeCycleId } = cyclesState;
+  const activeCycle = cycles.find((cycles) => cycles.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+    }
+
+    return 0;
+  });
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+
+    localStorage.setItem("@ignite-timer:cycles-state-1.0.0", stateJSON);
+  }, [cyclesState]);
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds);
+  }
+
+  function markCurrentCycleAsFinish() {
+    dispatch(markCurrentCycleAsFinishedAction());
+  }
+
+  function createNewCycle(data: CreateCycleDate) {
+    const id = String(new Date().getTime());
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    };
+
+    dispatch(addNewCycleAction(newCycle));
+
+    setAmountSecondsPassed(0);
+  }
+
+  function interruptCycle() {
+    dispatch(interruptCurrentCycleAction());
+  }
+
+  return (
+    <CyclesContext.Provider
+      value={{
+        cycles,
+        activeCycle,
+        activeCycleId,
+        markCurrentCycleAsFinish,
+        amountSecondsPassed,
+        setSecondsPassed,
+        createNewCycle,
+        interruptCycle,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  );
+}
---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,48 @@
+import { CyclesContext } from "../../../../contexts/CyclesContext";
+import { FormContainer } from "./styles";
+import { MinutesAmountInput, TaskInput } from "./styles";
+
+import { useContext } from "react";
+import { useFormContext } from "react-hook-form";
+
+
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        type="text"
+        list="task-suggestions"
+        id="task"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register("task")}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+        <option value="fruta" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register("minutesAmount", { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  );
+}
